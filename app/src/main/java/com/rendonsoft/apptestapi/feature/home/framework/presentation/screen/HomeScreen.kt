1f79decc92e5290055a821cc8997fe2fd5b8ce85
Luis Enrique Rendon Cortez <enriquerendonc@platzi.com>
2024-03-22 07:55:36 -0600

    Task  4: add loading component and state to show by state machine teh change into view

--- app/src/main/java/com/rendonsoft/apptestapi/feature/home/framework/presentation/screen/HomeScreen.kt
+++ app/src/main/java/com/rendonsoft/apptestapi/feature/home/framework/presentation/screen/HomeScreen.kt
@@ -1,19 +1,24 @@
 package com.rendonsoft.apptestapi.feature.home.framework.presentation.screen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.rendonsoft.apptestapi.R.string
 import com.rendonsoft.apptestapi.feature.home.framework.presentation.screen.composables.CardItem
+import com.rendonsoft.apptestapi.feature.home.framework.presentation.screen.state.HomeUiState.Data
+import com.rendonsoft.apptestapi.feature.home.framework.presentation.screen.state.HomeUiState.Loading
 import com.rendonsoft.apptestapi.feature.home.framework.presentation.view_model.HomeViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,11 +28,7 @@ fun HomePage(
     modifier: Modifier,
 ) {
 
-    val autosData by viewModel.autos.collectAsState(emptyList())
-
-    LaunchedEffect(Unit) {
-        viewModel.fetchAutos()
-    }
+    val state by viewModel.state.collectAsState()
 
     Scaffold(
         modifier = modifier,
@@ -39,12 +40,28 @@ fun HomePage(
             )
         }
     ) {
-        LazyColumn(modifier = modifier.padding(it)) {
-            items(autosData.size) { index ->
-                CardItem(
-                    item = autosData[index]
-                )
+
+        when (state) {
+            is Data -> {
+                val data = (state as Data)
+                LazyColumn(modifier = modifier.padding(it)) {
+                    items(data.autos.size) { index ->
+                        CardItem(
+                            item = data.autos[index]
+                        )
+                    }
+                }
+            }
+
+            Loading -> {
+                Box(modifier = modifier.padding(it)) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.align(Alignment.Center),
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                }
             }
         }
+
     }
 }
